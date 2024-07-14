c02dff527b2adb6587361407d1a12b1fc1d7f451
Siddhu31124 <158408126+Siddhu31124@users.noreply.github.com>
2024-07-14 16:12:12 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,17 @@
-
+import pandas as pd 
+import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plt
+ticker = 'ADANIENT.NS'
+df = yf.download(ticker, start='2023-07-10', end='2024-07-10', interval='1d')
+df['price']=df['Open'].shift(-1)
+df['prev_high']=df['High'].shift(1)
+df['prev_low']= df['Low'].shift(1)
+df['prev_close']=df['Close'].shift(1)
+df['gap_buy'] = np.where(
+    (df['Open'] > df['prev_high']) 
+| (((df['High'] - df['Close']) < 0.3 * (df['High'] - df['Low'])) & 
+((df['Open'] - df['Low']) < 0.3 * (df['High'] - df['Low']))),1,0)  
+df['gap_sell']=np.where((df['Open']<df['prev_low'])
+| (((df['High'] - df['Open']) < 0.3 * (df['High'] - df['Low'])) & 
+((df['Close'] - df['Low']) < 0.3 * (df['High'] - df['Low']))),1,0)
